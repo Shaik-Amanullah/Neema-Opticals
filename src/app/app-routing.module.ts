620180cea2054063c13f37d15c5b85afa8e0057f
vishnu <vishnu@walruspay.com>
2020-08-23 19:54:42 +0530

    basic routing and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { PageNotFoundComponent } from './common/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: "", redirectTo: '/home', pathMatch: 'full' },
+ { path: "home", component: HomeComponent },
+ { path: "product", component: ProductsComponent },
+ { path: "contact", component: ContactUsComponent },
+ { path: "about", component: AboutUsComponent },
+ { path: "**", component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { PageNotFoundComponent } from './common/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
